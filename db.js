e7a5742ca720755cec663fec72df09e172400b65
Uwe Wieben <u.wieben@cloud-user-skills.de>
2023-04-04 19:40:42 +0200

    Get Filter Defs

--- db.js
+++
@@ -1,6 +0,0 @@
-const {Pool} = require("pg")
-const {PGCONNECTIONSTRING} = process.env
-
-const pool = new Pool ({PGCONNECTIONSTRING})
-
-module.exports = pool;
--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./server")
-const port = 8000
-require('dotenv').config()
-
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-  })
